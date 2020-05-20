df22385bfea17bf6fa3679293d7b1f751e9036f4
dqf123 <59460057+dqf123@users.noreply.github.com>
2020-05-20 20:58:10 +0800

    Update Test3.java

--- test3/src/test3/Test3.java
+++ test3/src/test3/Test3.java
@@ -5,15 +5,11 @@ public class Test3 {
 
 	  String s ="abc";
 
-	  int b=10;
-          
-         int c=20;
 
-    int d=10;
-         int a=9;
+       
   System.out.println(s);
-  System.out.println(b);
-  System.out.println(a);
+  
+ 
 
 
 }
